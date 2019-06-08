41e20470cd89a170334ac57deafa965169223203
user <user@192.168.0.3>
2019-06-08 21:41:44 +0530

    Done

--- src/main/java/main/TicTacToe.java
+++ src/main/java/main/TicTacToe.java
@@ -1,10 +1,10 @@
 package main;
-
 import java.util.*;
-
-public class TicTacToe{
+public class TicTacToe
+{
 public static int[][] ticbox;
-	public static int win(int [][]tic) {
+	public static int win(int [][]tic)
+	{
 		//check all rows
 		//loop through rows from 0 to 3 and check if all the 3 places have same marks
  		
@@ -17,22 +17,48 @@ public static int[][] ticbox;
 		
 		
 		//write your code here !!!
+		for(int row = 0; row < 3; row ++)
+		{
+			if(tic[row][0] == 1 && tic[row][1] == 1 && tic[row][2] == 1)
+				return 1;
+			if(tic[row][0] == 2 && tic[row][1] == 2 && tic[row][2] == 2)
+				return 2;
+		}
 		
+		for(int col = 0; col < 3; col ++)
+		{
+			if(tic[0][col] == 1 && tic[1][col] == 1 && tic[2][col] == 1)
+				return 1;
+			if(tic[0][col] == 2 && tic[1][col] == 2 && tic[2][col] == 2)
+				return 2;
+		}
 		
+		if(tic[0][0] == 1 && tic[1][1] == 1 && tic[2][2] == 1)
+			return 1;
+		if(tic[0][0] == 2 && tic[1][1] == 2 && tic[2][2] == 2)
+			return 2;
+		if(tic[0][2] == 1 && tic[1][1] == 1 && tic[2][0] == 1)
+			return 1;
+		if(tic[0][2] == 2 && tic[1][1] == 2 && tic[2][0] == 2)
+			return 2;
 		
 		return 0;
 		
 	}
 	
-	public static void printBox(int [][]tic) {
-		for(int i=0;i<3;i++) {
-			for(int j=0;j<3;j++) {
+	public static void printBox(int [][]tic)
+	{
+		for(int i=0;i<3;i++) 
+		{
+			for(int j=0;j<3;j++) 
+			{
 				System.out.print(tic[i][j]);
 			}
 			System.out.println("");
 		}
 	}
-	public static boolean validate(int a1,int a2,int [][] tic) {
+	public static boolean validate(int a1,int a2,int [][] tic)
+	{
 		//check if a1 is between 0 & 3
 		//check if a2 is between 0 & 3
 		//check if the selected box is empty ie, already not marked by other player
@@ -40,7 +66,13 @@ public static int[][] ticbox;
 		
 		
 		//Write your code here !!!
-		
+		if( (0 <= a1 && a1 < 3) && (0 <= a2 && a2 < 3) )
+		{
+			if(tic[a1][a2] == 0)
+			{
+				return true;
+			}
+		}		
 		
 		return false;
 	}
